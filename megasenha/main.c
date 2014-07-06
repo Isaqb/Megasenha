2b20682a41fcb205de9c40e912527a6145330985
Isaqb <isaqbarbosa@gmail.com>
2014-07-06 14:31:47 -0300

    Update main.c

--- megasenha/main.c
+++ megasenha/main.c
@@ -1,17 +1,27 @@
+//!Main
+/*!Call interface_palavra_e_dica.h,interface_gui.h and *interface_controle.h.Start interface and show the winner player 
+*
+*
+*\author Douglas,Isabella,Thiago,Tiago
+*\since 04/07/14
+*\version 2.0
+*
+*
+*/
 #include "interface_palavra_e_dicas.h"
 #include "interface_gui.h"
 #include "interface_controle.h"
 
 
 int main(int argc, char *argv[]) {
-  int playerVencedor = 1;
-  int scoreFirstRound = 0;
+  int playerVencedor = 1;//!< Integer of the nunber og the winner player
+  int scoreFirstRound = 0;//!< Integer of the score of the first round
   
-  playerVencedor = iniciaPrimeiraFase(argc, argv, &scoreFirstRound);
+  playerVencedor = iniciaPrimeiraFase(argc, argv, &scoreFirstRound);//!< Receive the winner player
   closeInterface();
   updateInterface();
-  printf("o player vencedor e %d\n", playerVencedor);
-  iniciaSegundaFase(argc, argv, playerVencedor, scoreFirstRound);
+  printf("o player vencedor e %d\n", playerVencedor);//!< Show the winner player
+  iniciaSegundaFase(argc, argv, playerVencedor, scoreFirstRound);//!<Started the second round
 }
 
 // TODO: add interface to add new words
